2e99507d42e43b6982738300332878a8fff192ea
Prince-NKURUNZIZA <nkrnzzprince@gmail.com>
2021-12-11 22:21:51 +0100

    Generer le controleur DomaineFormation

---
+++ MyConservation/Controllers/DomaineFormationController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyConservation.Models;
+
+namespace MyConservation.Controllers
+{
+    public class DomaineFormationController : Controller
+    {
+        private ConservationEntities db = new ConservationEntities();
+
+        //
+        // GET: /DomaineFormation/
+
+        public ActionResult Index()
+        {
+            return View(db.DomaineFormations.ToList());
+        }
+
+        //
+        // GET: /DomaineFormation/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            DomaineFormation domaineformation = db.DomaineFormations.Find(id);
+            if (domaineformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(domaineformation);
+        }
+
+        //
+        // GET: /DomaineFormation/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /DomaineFormation/Create
+
+        [HttpPost]
+        public ActionResult Create(DomaineFormation domaineformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DomaineFormations.Add(domaineformation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(domaineformation);
+        }
+
+        //
+        // GET: /DomaineFormation/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            DomaineFormation domaineformation = db.DomaineFormations.Find(id);
+            if (domaineformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(domaineformation);
+        }
+
+        //
+        // POST: /DomaineFormation/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(DomaineFormation domaineformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(domaineformation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(domaineformation);
+        }
+
+        //
+        // GET: /DomaineFormation/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            DomaineFormation domaineformation = db.DomaineFormations.Find(id);
+            if (domaineformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(domaineformation);
+        }
+
+        //
+        // POST: /DomaineFormation/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DomaineFormation domaineformation = db.DomaineFormations.Find(id);
+            db.DomaineFormations.Remove(domaineformation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
