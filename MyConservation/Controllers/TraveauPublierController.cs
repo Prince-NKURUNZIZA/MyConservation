589ad16ba18a43ffc097162318ac0ea4fc791d8a
Prince-NKURUNZIZA <nkrnzzprince@gmail.com>
2022-01-18 15:25:57 +0100

    Ajout d'un controleur des traveau publies

--- MyConservation/Controllers/TraveauPublierController.cs
+++ MyConservation/Controllers/TraveauPublierController.cs
@@ -5,42 +5,25 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.IO;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using MyConservation.Models;
 
 namespace MyConservation.Controllers
 {
-    public class CompteEtudiantController : Controller
+    public class TraveauPublierController : Controller
     {
         private ConservationEntities db = new ConservationEntities();
 
         //
-        // GET: /CompteEtudiant/
+        // GET: /TraveauPublier/
 
         public ActionResult Index()
         {
-           
-
-            int idEtudiant =  0;
-            if(Session["idEtudiant"] != null)
-            {
-                idEtudiant =  Convert.ToInt32(Session["idEtudiant"].ToString());
-            }
-
-            var documents = db.Documents.Include(d => d.Diplome1).Include(d => d.DomaineFormation).Include(d => d.NatureDocument).Include(d => d.Etudiant).Include(d => d.Universite1).Where(d => d.nomAuteur == idEtudiant).OrderByDescending(d=>d.id);
-
-
+            var documents = db.Documents.Include(d => d.Diplome1).Include(d => d.DomaineFormation).Include(d => d.NatureDocument).Include(d => d.Etudiant).Include(d => d.Universite1);
             return View(documents.ToList());
-        }
-        public ActionResult Acceuil()
-        {
-            return View();
         }
 
         //
-        // GET: /CompteEtudiant/Details/5
+        // GET: /TraveauPublier/Details/5
 
         public ActionResult Details(int id = 0)
         {
@@ -53,46 +36,28 @@ namespace MyConservation.Controllers
         }
 
         //
-        // GET: /CompteEtudiant/Create
+        // GET: /TraveauPublier/Create
 
         public ActionResult Create()
         {
-
-
             ViewBag.diplome = new SelectList(db.Diplomes, "id", "niveau");
             ViewBag.domaine = new SelectList(db.DomaineFormations, "id", "nomDomaine");
             ViewBag.nature = new SelectList(db.NatureDocuments, "id", "nature");
             ViewBag.nomAuteur = new SelectList(db.Etudiants, "id", "nom");
             ViewBag.universite = new SelectList(db.Universites, "id", "nomUniversite");
-
             return View();
-
         }
 
         //
-        // POST: /CompteEtudiant/Create
-
+        // POST: /TraveauPublier/Create
 
         [HttpPost]
-        public ActionResult Create(Document document, HttpPostedFileBase fichier)
+        public ActionResult Create(Document document)
         {
-            string name=null;
-            string fileName = null;
             if (ModelState.IsValid)
             {
-                if (fichier != null)
-                {
-                    fileName = Path.GetFileName(fichier.FileName);
-                    name = Path.GetFileNameWithoutExtension(fileName);
-                    string path = Path.Combine(Server.MapPath("~/Fichiers"), Path.GetFileName(fichier.FileName));
-                    fichier.SaveAs(path);
-
-                }
-                document.fichier = fileName;
                 db.Documents.Add(document);
-
                 db.SaveChanges();
-                TempData["AlertMessage"] = "Enregistre avec sucess....!";
                 return RedirectToAction("Index");
             }
 
@@ -103,30 +68,9 @@ namespace MyConservation.Controllers
             ViewBag.universite = new SelectList(db.Universites, "id", "nomUniversite", document.universite);
             return View(document);
         }
-        //////////////////////////////////////////////////////////
-
-
-
-        public ActionResult Downloads()
-        {
-            var dir = new System.IO.DirectoryInfo(Server.MapPath("~/Fichiers/"));
-            System.IO.FileInfo[] fileNames = dir.GetFiles("*.*"); List<string> items = new List<string>();
-            foreach (var file in fileNames)
-            {
-                items.Add(file.Name);
-            }
-            return View(items);
-        }
- 
-        public FileResult Download(string ImageName)
-        {
-            var FileVirtualPath = "~/Fichiers/" + ImageName;
-            return File(FileVirtualPath, "application/force-download", Path.GetFileName(FileVirtualPath));
-        }
-
 
         //
-        // GET: /CompteEtudiant/Edit/5
+        // GET: /TraveauPublier/Edit/5
 
         public ActionResult Edit(int id = 0)
         {
@@ -144,7 +88,7 @@ namespace MyConservation.Controllers
         }
 
         //
-        // POST: /CompteEtudiant/Edit/5
+        // POST: /TraveauPublier/Edit/5
 
         [HttpPost]
         public ActionResult Edit(Document document)
@@ -153,7 +97,6 @@ namespace MyConservation.Controllers
             {
                 db.Entry(document).State = EntityState.Modified;
                 db.SaveChanges();
-                TempData["AlertMessage"] = "Modifié avec sucess....!";
                 return RedirectToAction("Index");
             }
             ViewBag.diplome = new SelectList(db.Diplomes, "id", "niveau", document.diplome);
@@ -165,7 +108,7 @@ namespace MyConservation.Controllers
         }
 
         //
-        // GET: /CompteEtudiant/Delete/5
+        // GET: /TraveauPublier/Delete/5
 
         public ActionResult Delete(int id = 0)
         {
@@ -178,7 +121,7 @@ namespace MyConservation.Controllers
         }
 
         //
-        // POST: /CompteEtudiant/Delete/5
+        // POST: /TraveauPublier/Delete/5
 
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
@@ -186,7 +129,6 @@ namespace MyConservation.Controllers
             Document document = db.Documents.Find(id);
             db.Documents.Remove(document);
             db.SaveChanges();
-            TempData["AlertMessage"] = "Supprimé avec sucess....!";
             return RedirectToAction("Index");
         }
 
